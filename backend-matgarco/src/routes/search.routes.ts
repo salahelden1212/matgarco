a71a2245c6d79a2e91259e338b08c61ec9aa305a
salahelden1212 <salahelkalyouby2@gmail.com>
2026-06-14 02:56:14 +0300

    hjhg

--- backend-matgarco/src/routes/search.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { authenticate, isMerchant } from '../middleware/auth.middleware';
-import { globalSearch } from '../controllers/search.controller';
-
-const router = Router();
-
-router.get('/', authenticate, isMerchant, globalSearch);
-
-export default router;
